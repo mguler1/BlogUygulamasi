ff31d25e38ef0be1e3d4832099c26cb2ebb442e2
= <=>
2022-07-14 11:41:17 +0300

    Blog ve detayda imageler düzenlendi

---
+++ BlogUygulaması.Web/Enums/BlogImageType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogUygulaması.Web.Enums
+{
+    public enum BlogImageType
+    {
+        BlogHome=1,
+        BlogDetail=2
+    }
+}
--- BlogUygulaması.Web/TagHelpers/ImageTagHelper.cs
+++ BlogUygulaması.Web/TagHelpers/ImageTagHelper.cs
@@ -1,4 +1,5 @@
 ﻿using BlogUygulaması.Web.ApiServices.Interfaces;
+using BlogUygulaması.Web.Enums;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
@@ -16,10 +17,19 @@ namespace BlogUygulaması.Web.TagHelpers
             _imageApiService = imageApiService;
         }
         public int Id { get; set; }
+        public BlogImageType BlogImageType { get; set; } = BlogImageType.BlogHome;
         public override async  Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
           var blog=await _imageApiService.GetBlogImageByIdAsync(Id);
-            var html = $"<img src='{blog}'class='card - img - top'/>";
+            string html=string.Empty;
+            if (BlogImageType== BlogImageType.BlogHome)
+            {
+                html = $"<img src='{blog}'class='card - img - top'/>";
+            }
+            else
+            {
+                html = $"<img src='{blog}'class='card - img -fluid rounded'/>";
+            }
             output.Content.SetHtmlContent(html);
         }
     }
